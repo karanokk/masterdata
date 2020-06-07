2aaa36ff0c81587ec81950b32b54d761c2e4e67b
karanokk <karanokk@icloud.com>
2020-06-07 16:50:13 +0800

    refactor(kazemai): turn into `kazemai` module

--- masterdata/MasterData.py
+++ masterdata/MasterData.py
@@ -3,6 +3,7 @@ import logging
 from os import path
 
 from . import kazemai
+from . import mooncell
 from .json2sqlite3 import JSDatabase
 
 logger = logging.getLogger('masterdata')
@@ -12,33 +13,17 @@ class MasterData:
     def __init__(self, ver_file: str):
         self.ver_file = ver_file
 
-    def make(self, path: str):
-        new_id = self.latest_master_js_id()
+    def make_jp_database(self, path: str):
+        new_id = kazemai.get_lastest_master_js_id()
         old_id = self.read_id()
         logger.info(f'new id: [{new_id}]\n old id: [{old_id}]')
         if old_id == new_id:
             logger.info(f'Update to date, quit')
             return
-            
-        logger.info('Fetching masterdata...')
-        master_json = kazemai.fetch_masterdata()
-        logger.info('Making database...')
-        database = JSDatabase(path)
-        database.load_json(master_json)
-        sql = "SELECT id FROM mstSvt WHERE (type=1 or type=2) and collectionNo>0;"
-        servant_ids = [i[0] for i in database.execute(sql)]
-        logger.info('Fetching comments...')
-        comments = kazemai.fetch_svtcomment(servant_ids)
-        mstSvtComment = {'mstSvtComment': comments}
-        logger.info('Add comments...')
-        database.load_json(mstSvtComment)
+        kazemai.make_database(path)
         self.write_id(new_id)
         logger.info(f'Write new id [{new_id}]')
         logger.info('Complete!')
-
-    def latest_master_js_id(self) -> str:
-        latest_id = kazemai.get_lastest_master_js_id()
-        return latest_id
 
     def read_id(self) -> str:
         with open(self.ver_file) as f:
